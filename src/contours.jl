57b9ea326c0ec44525c8accf0aa1dec8136cbaf9
Samuel Watson <samuel.s.watson@gmail.com>
2019-09-19 19:05:10 -0400

    Fix isolines docstring

--- src/contours.jl
+++ src/contours.jl
@@ -29,7 +29,7 @@ const _DEFAULT_ISOLINE_KWARGS =
     Plot the contour lines of the function whose values are represented
     by the array (or function) `zs`. If `lift` is true, plot in 3D.
     
-# Example
+# Examples
 ```julia-repl
 julia> isolines(0:10, 0:10, (x,y) -> (100 - x^2 + y^2)/10, lift = true)
 """

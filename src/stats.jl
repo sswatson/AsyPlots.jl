1639e0526a1a0567d6c63bf8b8b569a6b6f1b3af
Samuel Watson <samuel.s.watson@gmail.com>
2020-09-30 19:49:50 -0400

    update stats.jl

--- src/stats.jl
+++ src/stats.jl
@@ -52,12 +52,12 @@ function piechart(labels,frequencies; title="")
         error("Labels and frequencies should have the same length")
     end
     angles = [[0];cumsum(2π*frequencies/sum(frequencies))]
-    fontsizes = [min(12,round(Integer;digits=50*(angles[i+1]-angles[i]))) for i=1:length(angles)-1]
+    fontsizes = [min(12,round(Integer, 50*(angles[i+1]-angles[i]))) for i=1:length(angles)-1]
     Random.seed!(1)
     sectors = [sector(1,angles[i],angles[i+1];
                       fillpen=Pen(color=ColorTypes.RGB(rand(),rand(),rand()),opacity=0.4))
                for i=1:length(angles)-1]
-    text = [Label("$(labels[i]) ($(string(frequencies[i])))",(1+fontsizes[i]/60)*cis(mean([angles[i],angles[i+1]]));
+    text = [Label("$(labels[i]) ($(string(frequencies[i])))",(1+fontsizes[i]/60)*cis(Statistics.mean([angles[i],angles[i+1]]));
                   fontsize=fontsizes[i]) for i=1:length(angles)-1]
     if title == ""
         return Plot(sectors) + Plot(text;width=512)
@@ -67,7 +67,7 @@ function piechart(labels,frequencies; title="")
 end
 
 function sector(r,θ1,θ2;kwargs...)
-    arc = [r*cis(θ) for θ=linspace(θ1,θ2,500)[2:end-1]]
+    arc = [r*cis(θ) for θ=range(θ1,stop=θ2,length=500)[2:end-1]]
     vee = [r*cis(θ2),0,r*cis(θ1)]
     Polygon(vcat(arc,vee);kwargs...)
 end
